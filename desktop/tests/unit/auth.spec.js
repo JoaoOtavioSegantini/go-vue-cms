8ea10534c470d69528310eab20a7719d84e71629
João Otávio <joaotavioos@outlook.com>
2022-10-31 06:59:18 -0300

    test: update auth component test 🚨

--- desktop/tests/unit/auth.spec.js
+++ desktop/tests/unit/auth.spec.js
@@ -1,17 +1,49 @@
 import Auth from "@/components/Auth.vue";
 import { mount } from "@vue/test-utils";
 
+// const sinon = require('sinon');
+
+// const mockRequest = () => {
+//   return {
+//     users: []
+//   };
+// };
+
+// const mockResponse = () => {
+//   const res = {};
+//   res.status = sinon.stub().returns(res);
+//   res.json = sinon.stub().returns(res);
+//   return res;
+// };
+
+window.axios = {
+  post: jest
+    .fn(() => {
+      return {
+        then: jest.fn(() => "your faked response"),
+      };
+    })
+    .mockName("axiosPost"),
+};
+
 describe("Auth.vue", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   function factory() {
     return mount(Auth, {
-      // data() {
-      //   return {
-      //     user: {
-      //       email: "acbdfg@gmail.com",
-      //       password: "987654"
-      //     },
-      //   };
-      // },
+      mocks: {
+        axios: window.axios,
+      },
+      data() {
+        return {
+          user: {
+            email: "acbdfg@gmail.com",
+            password: "987654",
+          },
+        };
+      },
     });
   }
 
@@ -19,8 +51,24 @@ describe("Auth.vue", () => {
     const msg = "Autenticação Informe suas credenciaisEmailSenhaAcessar";
 
     const wrapper = factory();
+
     console.log(wrapper.vm);
-    // expect(wrapper.vm.auth()).toHaveBeenCalled();
+    wrapper.findAll("input")[0].setValue("acbdfg@gmail.com");
+    wrapper.findAll("input")[1].setValue("987654");
+
+    await wrapper.find("form").trigger("submit");
+    console.log(wrapper.emitted());
+
+    await wrapper.vm.auth();
+
+    expect(window.axios.post).toBeCalledWith(
+      "/api/token",
+      { email: "acbdfg@gmail.com", password: "987654" },
+      {
+        swalMessage: "Você acessou com sucesso",
+        swalTitle: "Autenticado com sucesso",
+      }
+    );
     expect(wrapper.text()).toMatch(msg);
     expect(wrapper.html()).toMatchSnapshot();
   });
