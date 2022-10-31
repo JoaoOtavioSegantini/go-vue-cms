02ffae27bdcd91fc6f2781718b8ba778ca22da2a
João Otávio <joaotavioos@outlook.com>
2022-10-31 04:36:36 -0300

    feat: implements home show controller ✨

--- server/controller/home-controller.go
+++ server/controller/home-controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joaotavioos/cms-server/database"
@@ -11,11 +12,28 @@ import (
 func Home(context *gin.Context) {
 	var page entity.PageMysql
 	var menus []entity.PageMysql
+	var posts []entity.PostMysql
+	var total int64
+
+	offset := context.Request.URL.Query().Get("page")
+
+	if offset == "" {
+		offset = "0"
+	}
+
+	pagination, err := strconv.Atoi(offset)
+
+	if err != nil {
+		context.Abort()
+		context.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
 
 	database.Instance.Model(&page).First(&page)
 	database.Instance.Find(&menus)
+	database.Instance.Model(&posts).Count(&total).Offset(pagination).Limit(10).Find(&posts)
 
-	context.HTML(http.StatusOK, "page.html", gin.H{"page": page, "menus": menus})
+	context.HTML(http.StatusOK, "page.html", gin.H{"page": page, "menus": menus, "posts": posts})
 
 }
 
@@ -24,6 +42,21 @@ func View(context *gin.Context) {
 
 	slug := context.Param("slug")
 	database.Instance.Model(&page).Where("Slug = ?", slug).Find(&page)
+	var menus []entity.PageMysql
 
-	context.HTML(http.StatusOK, "view.html", gin.H{"page": page})
+	database.Instance.Find(&menus)
+	context.HTML(http.StatusOK, "view.html", gin.H{"page": page, "menus": menus})
+}
+
+func Show(context *gin.Context) {
+	var menus []entity.PageMysql
+	var page entity.PageMysql
+	var post entity.PostMysql
+
+	database.Instance.Find(&menus)
+
+	slug := context.Param("slug")
+	database.Instance.Model(&post).Where("Slug = ?", slug).Find(&post)
+	context.HTML(http.StatusOK, "articles.html", gin.H{"page": page, "menus": menus, "post": post})
+
 }
