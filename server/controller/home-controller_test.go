6ffb21215548478be2ca74f60b43cdd252c1a850
João Otávio <joaotavioos@outlook.com>
2022-11-02 21:47:18 -0300

    test: finish home controller tests 🚨

--- server/controller/home-controller_test.go
+++ server/controller/home-controller_test.go
@@ -87,3 +87,179 @@ func TestHomeRoute(t *testing.T) {
 	assert.Contains(t, w.Body.String(), "<a href=\"/home/articles/a-simple-test-title-for-post-2\" class=\"btn btn-primary\">Ler mais >></a>\n")
 	assert.Contains(t, w.Body.String(), "<a href=\"/home/articles/a-simple-test-title-for-post-3\" class=\"btn btn-primary\">Ler mais >></a>\n")
 }
+
+func TestViewRouteFirst(t *testing.T) {
+	t.Parallel()
+	//	database.Connect("root:root@tcp(localhost:3306)/cms-database?parseTime=true")
+	database.Connect("")
+
+	database.Migrate()
+
+	var (
+		page, page1, page2, page3 entity.PageMysql
+	)
+
+	page.Title = "Home"
+	page.Slug = slug.Make(page.Title)
+	page1.Title = "Links"
+	page1.Slug = slug.Make(page1.Title)
+	page1.Body = "<p>&nbsp;</p><figure class=\"image\"><img src=\"http://localhost:8000/uploads/undraw_writer_q06d.png\"></figure><p><strong>Um blog sobre assuntos diversos, tais como tecnologia, ciência e leis.</strong></p>"
+	page2.Title = "Sobre nós"
+	page2.Slug = slug.Make(page2.Title)
+	page2.Body = "a simple text body"
+	page3.Title = "Fale conosco"
+	page3.Slug = slug.Make(page3.Title)
+	page3.Body = "a simple test text body"
+
+	database.Instance.Create(&page)
+	database.Instance.Create(&page1)
+	database.Instance.Create(&page2)
+	database.Instance.Create(&page3)
+
+	w := httptest.NewRecorder()
+	ctx, r := gin.CreateTestContext(w)
+
+	routes.SetupRouter(r, "../templates/*.html")
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "/home/links", nil)
+	if err != nil {
+		t.Errorf("got error: %s", err)
+	}
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), "<title>Links</title>")
+	assert.Contains(t, w.Body.String(), page1.Body)
+	assert.NotContains(t, w.Body.String(), "<title>Sobre nós</title>")
+	assert.NotContains(t, w.Body.String(), page2.Body)
+	assert.NotContains(t, w.Body.String(), "<title>Fale conosco</title>")
+	assert.NotContains(t, w.Body.String(), page3.Body)
+
+	assert.Contains(t, w.Body.String(), "<a href=\"/home\" class=\"logo\">Segantini's Blog</a>")
+	//	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home\" class=\"nav-link\">Home</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/links\" class=\"nav-link active\">Links</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/fale-conosco\" class=\"nav-link\">Fale conosco</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/sobre-nos\" class=\"nav-link\">Sobre nós</a></li>")
+
+}
+
+func TestViewRouteSecond(t *testing.T) {
+	t.Parallel()
+	//	database.Connect("root:root@tcp(localhost:3306)/cms-database?parseTime=true")
+	database.Connect("")
+
+	database.Migrate()
+
+	var (
+		page, page1, page2, page3 entity.PageMysql
+	)
+
+	page.Title = "Home"
+	page.Slug = slug.Make(page.Title)
+	page1.Title = "Links"
+	page1.Slug = slug.Make(page1.Title)
+	page1.Body = "<p>&nbsp;</p><figure class=\"image\"><img src=\"http://localhost:8000/uploads/undraw_writer_q06d.png\"></figure><p><strong>Um blog sobre assuntos diversos, tais como tecnologia, ciência e leis.</strong></p>"
+	page2.Title = "Sobre nós"
+	page2.Slug = slug.Make(page2.Title)
+	page2.Body = "a simple text body"
+	page3.Title = "Fale conosco"
+	page3.Slug = slug.Make(page3.Title)
+	page3.Body = "a simple test text body"
+
+	database.Instance.Create(&page)
+	database.Instance.Create(&page1)
+	database.Instance.Create(&page2)
+	database.Instance.Create(&page3)
+
+	w := httptest.NewRecorder()
+	ctx, r := gin.CreateTestContext(w)
+
+	routes.SetupRouter(r, "../templates/*.html")
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "/home/"+page2.Slug, nil)
+	if err != nil {
+		t.Errorf("got error: %s", err)
+	}
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), "<title>Sobre nós</title>")
+	assert.Contains(t, w.Body.String(), page2.Body)
+	assert.NotContains(t, w.Body.String(), "<title>Links</title>")
+	assert.NotContains(t, w.Body.String(), page1.Body)
+	assert.NotContains(t, w.Body.String(), "<title>Fale conosco</title>")
+	assert.NotContains(t, w.Body.String(), page3.Body)
+
+	assert.Contains(t, w.Body.String(), "<a href=\"/home\" class=\"logo\">Segantini's Blog</a>")
+	//	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home\" class=\"nav-link\">Home</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/links\" class=\"nav-link\">Links</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/fale-conosco\" class=\"nav-link\">Fale conosco</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/sobre-nos\" class=\"nav-link active\">Sobre nós</a></li>")
+
+}
+
+func TestShowRoute(t *testing.T) {
+	t.Parallel()
+	//	database.Connect("root:root@tcp(localhost:3306)/cms-database?parseTime=true")
+	database.Connect("")
+
+	database.Migrate()
+
+	var (
+		page, page1, page2, page3 entity.PageMysql
+		post                      entity.PostMysql
+	)
+
+	page.Title = "Home"
+	page.Slug = slug.Make(page.Title)
+	page1.Title = "Links"
+	page1.Slug = slug.Make(page1.Title)
+	page1.Body = "<p>&nbsp;</p><figure class=\"image\"><img src=\"http://localhost:8000/uploads/undraw_writer_q06d.png\"></figure><p><strong>Um blog sobre assuntos diversos, tais como tecnologia, ciência e leis.</strong></p>"
+	page2.Title = "Sobre nós"
+	page2.Slug = slug.Make(page2.Title)
+	page2.Body = "a simple text body"
+	page3.Title = "Fale conosco"
+	page3.Slug = slug.Make(page3.Title)
+	page3.Body = "a simple test text body"
+
+	database.Instance.Create(&page)
+	database.Instance.Create(&page1)
+	database.Instance.Create(&page2)
+	database.Instance.Create(&page3)
+
+	post.Title = "A simple test title for first post"
+	post.Slug = slug.Make(post.Title)
+	post.Body = "<div><strong>A simple post title</strong> <p> A simple post introduction </p></div>"
+
+	database.Instance.Create(&post)
+
+	w := httptest.NewRecorder()
+	ctx, r := gin.CreateTestContext(w)
+
+	routes.SetupRouter(r, "../templates/*.html")
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "/home/articles/"+post.Slug, nil)
+	if err != nil {
+		t.Errorf("got error: %s", err)
+	}
+
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), post.Body)
+	assert.Contains(t, w.Body.String(), "<title>"+post.Title+"</title>")
+	assert.NotContains(t, w.Body.String(), "<title>Sobre nós</title>")
+	assert.NotContains(t, w.Body.String(), "<title>Links</title>")
+	assert.NotContains(t, w.Body.String(), page1.Body)
+	assert.NotContains(t, w.Body.String(), "<title>Fale conosco</title>")
+	assert.NotContains(t, w.Body.String(), page3.Body)
+
+	assert.Contains(t, w.Body.String(), "<a href=\"/home\" class=\"logo\">Segantini's Blog</a>")
+	//assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home\" class=\"nav-link\">Home</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/links\" class=\"nav-link\">Links</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/fale-conosco\" class=\"nav-link\">Fale conosco</a></li>")
+	assert.Contains(t, w.Body.String(), "<li class=\"nav-item\"><a href=\"/home/sobre-nos\" class=\"nav-link\">Sobre nós</a></li>")
+
+}
